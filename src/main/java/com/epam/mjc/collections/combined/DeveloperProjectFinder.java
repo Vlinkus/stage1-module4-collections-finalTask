0910450ab6b0d5ca73aca23bf69c2490586c5b79
VLinkus <LinkusVyte@gmail.com>
2023-12-15 10:11:53 +0200

    M4 Collections Final Task Completed

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,27 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> projectList = new ArrayList<>();
+        projects.forEach((key,value) -> {
+            if(value.contains(developer))
+                projectList.add(key);
+        });
+        projectList.sort(new reversedListByStringLengthComparator());
+        return projectList;
     }
+
 }
+
+ class reversedListByStringLengthComparator implements Comparator<String> {
+     @Override
+     public int compare(String a, String b) {
+         if(a.length() != b.length()){
+             return  Integer.compare(b.length(), a.length());
+         } else {
+             return b.compareTo(a);
+         }
+     }
+ }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,18 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> createdMap = new HashMap<>();
+        sourceMap.forEach((key, value) -> {
+            Set<String> mySet = createdMap.containsKey(key.length()) ? createdMap.get(key.length()) : new HashSet<>();
+            mySet.add(key);
+            createdMap.put(key.length(), mySet);
+        });
+        return createdMap;
     }
 }
